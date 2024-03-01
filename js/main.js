e0274faa709a5f58a33f39bb85ebdacbeb6b6c5d
Artur-Neri <arturnerictt@gmail.com>
2024-03-01 17:42:42 -0300

    start using js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const toggle = document.querySelector('#js-ld-toggle');
 
+let boards = document.querySelectorAll('.sidebar-options__option');
+
+// TOGGLE LIGHT AND DARK MODE
 toggle.onclick = function(){
     this.classList.toggle('jc-e');
 }
+
+boards.forEach((board) => {
+    // STYLES BOARDS HOVER IN AND OUT
+    board.onmouseover = ()=>{
+        board.style.backgroundColor = '#A8A4FF';
+        board.style.color = '#ffffff';
+        board.children[0].src = './assets/fluent_board-split-white.png';
+    }
+    board.onmouseout = ()=>{
+        board.style.backgroundColor = '';
+        board.style.color = '#828FA3';
+        board.children[0].src = './assets/fluent_board-split-grey.png';
+    }
+
+})
