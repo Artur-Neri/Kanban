c0bc9a56e11e582205dd581cd7d1c346b8fa9e17
Artur-Neri <arturnerictt@gmail.com>
2024-03-01 20:51:16 -0300

    estilizando componentes da tela inicial

--- js/main.js
+++ js/main.js
@@ -10,14 +10,46 @@ toggle.onclick = function(){
 boards.forEach((board) => {
     // STYLES BOARDS HOVER IN AND OUT
     board.onmouseover = ()=>{
-        board.style.backgroundColor = '#A8A4FF';
-        board.style.color = '#ffffff';
-        board.children[0].src = './assets/fluent_board-split-white.png';
+
+        if (board.classList.contains('sidebar-options__option--selected')) {
+            console.log('está ativo');
+        } else {
+            board.style.backgroundColor = '#A8A4FF';
+            board.style.color = '#ffffff';
+            board.children[0].src = './assets/fluent_board-split-white.png';
+        }
+
     }
     board.onmouseout = ()=>{
-        board.style.backgroundColor = '';
-        board.style.color = '#828FA3';
-        board.children[0].src = './assets/fluent_board-split-grey.png';
+
+        if (board.classList.contains('sidebar-options__option--selected')) {
+            console.log('está ativo');
+        } else {
+            board.style.backgroundColor = '';
+            board.style.color = '#828FA3';
+            board.children[0].src = './assets/fluent_board-split-grey.png';
+        }
+
+    }
+
+    // STYLES BOARDS TO ACTIVE
+
+    board.onclick = () => {
+        inativaTodosBoards();
+        board.style.backgroundColor = '#635FC7';
+        board.style.color = '#ffffff';
+        board.children[0].src = './assets/fluent_board-split-white.png';
+        board.classList.toggle('sidebar-options__option--selected');
+        console.log('toggle');
     }
 
 })
+
+function inativaTodosBoards() {
+    boards.forEach((board) => {
+        board.style.backgroundColor = '';
+        board.style.color = '#828FA3';
+        board.children[0].src = './assets/fluent_board-split-grey.png';
+        board.classList.remove('sidebar-options__option--selected');
+    })
+}
